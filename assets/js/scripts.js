7fc461edc384f0eaa6bc988a87a79cbc1ba452cf
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-12-04 20:32:09 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -86,38 +86,38 @@ $(document).ready(function() {
 
 // data table
 
-// let table = new DataTable('#myTable');
-// $(document).ready(function () {
-//   $('#myTable').DataTable({
-//     paging: false
-//   });
-//   $('#myTable2').DataTable({
-//     paging: false
-//   });
-//   $('#myTable3').DataTable({
-//     paging: false
-//   });
-//   $('#myTable4').DataTable({
-//     paging: false
-//   });
-// });
+let table = new DataTable('#myTable');
+$(document).ready(function () {
+  $('#myTable').DataTable({
+    paging: false
+  });
+  $('#myTable2').DataTable({
+    paging: false
+  });
+  $('#myTable3').DataTable({
+    paging: false
+  });
+  $('#myTable4').DataTable({
+    paging: false
+  });
+});
 
 
 // datapicker
 
-// jalaliDatepicker.startWatch();
+jalaliDatepicker.startWatch();
 
 // sweet alert
 
 // let submitForm = document.getElementById('submit');
 
-// submitForm.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   Swal.fire({
-//     position: 'top-center',
-//     icon: 'success',
-//     title: 'با موفقیت ثبت شد',
-//     showConfirmButton: false,
-//     timer: 3000
-//   })
-// })  
+submitForm.addEventListener('click', function (e) {
+  e.preventDefault();
+  Swal.fire({
+    position: 'top-center',
+    icon: 'success',
+    title: 'با موفقیت ثبت شد',
+    showConfirmButton: false,
+    timer: 3000
+  })
+})  
